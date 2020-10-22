facecdd12d584c1db59f4450522eac3225c3045c
AswithaReddy <aswithareddy24@gmail.com>
2020-10-22 22:38:05 +0530

    my first commit

---
+++ LabAssignment_1/FibonacciWithoutRecursionAssignment.java
@@ -0,0 +1,28 @@
+//Desc: Fibonacci without Recursion
+//@author: Pankaj
+
+import java.util.Scanner;
+
+public class FibonacciWithoutRecursionAssignment {
+	static int fibonacci(int num)
+	{
+	    int a=1,b=1,c;
+	    for(int i=0;i<num-2;i++)
+	       {
+	          c=a+b;
+	          a=b;
+	          b=c;
+	       }
+	    return b;
+	}
+
+public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int num=sc.nextInt();
+			    System.out.println(fibonacci(num));
+	  }
+
+    }
+			
+}
+
---
+++ LabAssignment_1/numberIsPowerOfTwo.java
@@ -0,0 +1,28 @@
+//Desc: The given number is power of 2
+//@author: Pankaj
+
+class numberIsPowerOfTwo {
+	public static int checkNumber(int n) {
+	        if(n == 0) 
+	        return 0; 
+	        while(n != 1) 
+	        {  
+	            if(n % 2 != 0) 
+	            return 0; 
+	            n = n / 2;      
+	        } 
+	        return 1; 
+	    }  
+	public static void main(String[] args) 
+    { 
+         
+		int n=8;
+		if(checkNumber(n) == 1) 
+         System.out.println(n +  
+                           " is a power of 2"); 
+        else
+         System.out.println(n +  
+                           " is not a power of 2"); 
+    } 
+} 
+
